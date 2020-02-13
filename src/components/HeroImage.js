01e88f35ec8b616f3114154911d5ce3b1c41afb4
Addie Padula <rebekah.adair@yahoo.com>
2020-02-13 10:57:47 -0500

    works on functionality and styling of video play button

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import promoVideo from '../assets/Under_The_Sun_Promo.mp4';
-
+import playIcon from '../assets/icon-play.svg';
 import bandPhoto from '../assets/under-the-sun-1.jpg';
 
 const HeroImage = styled.img`
@@ -17,6 +17,7 @@ export default (props) => {
             <video className="video" muted="true" controls="true" fullscreen="allow">
                 <source src={promoVideo} type="video/mp4"/>
             </video>
+            <button className="video-button" aria-label="video play/pause button"><img className="video-button__image" src={playIcon}/></button>
         </div>
 
         // <HeroImage src={bandPhoto}/>
