816bf7c95acfcd1df01f1952c62d36ddce96fa20
Addie Padula <rebekah.adair@yahoo.com>
2020-02-13 16:21:43 -0500

    adds video please work lfs

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// import promoVideo from '../assets/Under_The_Sun_Promo.mp4';
+import promoVideo from '../assets/Under_The_Sun_Promo.mp4';
 import playIcon from '../assets/icon-play.svg';
 import bandPhoto from '../assets/under-the-sun-1.jpg';
 
@@ -15,7 +15,7 @@ export default (props) => {
     return (
         <div className="video-wrapper">
             <video className="video" controls="true" fullscreen="allow">
-                {/* <source src={promoVideo} type="video/mp4"/> */}
+                <source src={promoVideo} type="video/mp4"/>
             </video>
             <button className="video-button" aria-label="video play/pause button"><img className="video-button__image" src={playIcon}/></button>
         </div>
