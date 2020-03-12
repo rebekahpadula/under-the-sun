ccb834c15d7db31ee23435a16f45e866b9c25d5b
Addie Padula <rebekah.adair@yahoo.com>
2020-03-12 12:05:09 -0400

    edits video current time stamp function

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
 
-document.querySelector('.video').addEventListener('loadedmetadata', function() {
-    this.currentTime = 1.2;
-  }, false);
+// document.querySelector('.video').addEventListener('loadedmetadata', function() {
+//     this.currentTime = 1.2;
+//   }, false);
+
+document.querySelector('.video').addEventListener('canplaythrough', function() {
+  this.currentTime = 1.2;
+}, false);
+
